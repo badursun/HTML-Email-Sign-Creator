5a6bfe4a7d37dd68816b7996ff99ec2c24f53da1
Anthony Burak DURSUN <badursun@gmail.com>
2022-02-01 11:32:13 +0300

    Update
    Update

--- js/app.js
+++ js/app.js
@@ -16,11 +16,10 @@ jQuery(document).ready(function($) {
 	      	return false;
 	    };
 	    
-	    let FileName = ''+ $('#ADSOYAD').val();
 	    let htmlContent = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>'+ $('#ONIZLEME').html() +'</body></html>';
 	    let blob = new Blob([htmlContent], { type: "text/html;charset=utf-8" });
 	    
-	    	saveAs(blob, convertToSlug(FileName)+'.html');
+	    	saveAs(blob, convertToSlug($('#ADSOYAD').val()) + '.html');
 	});
 
 	/*
@@ -33,26 +32,40 @@ jQuery(document).ready(function($) {
 			Swal.fire('Attention','Select theme first!','error');
 			return false;
 		};
+		if( $('#ADSOYAD').val().length < 2 ){
+			Swal.fire('Attention','Write a name','error');
+			return false;
+		};
+		if( $('#TITLE').val().length < 2 ){
+			Swal.fire('Attention','Write a title','error');
+			return false;
+		};
+		if( $('#EPOSTA').val().length < 2 ){
+			Swal.fire('Attention','Write a email','error');
+			return false;
+		};
+		if( $('#ADRES').val().length < 2 ){
+			Swal.fire('Attention','Write a address','error');
+			return false;
+		};
 
 		$('.download-signatura, #PREVIEW').prop('disabled', true);
+		
 		$('#ONIZLEME').html('<center><img src="img/loading.svg" class="img-fluid" style="height:100px;" /><h5>Generating...</h5></center>')
 		
 		let $Data 	= objectifyForm( $('#DATA').serializeArray() );
-		let $Sirket = $('option:selected', $('#SIRKET'));
-		let $Host 	= $('#HOST').val();
 		let $Domain = $('option:selected', $('#SIRKET')).attr('data-domain');
 
 		let $TemplateData = {
 			ADSOYAD 		: $Data.ADSOYAD,
 			TITLE 			: $Data.TITLE,
-			EPOSTA 			: ($Data.EPOSTA.length>1 ? $Data.EPOSTA + '@' + $Domain : ''),
+			EPOSTA 			: $Data.EPOSTA,
 			MOBIL_TELEFON 	: $Data.CEPTELEFONU,
 			SIRKET_TELEFON 	: $Data.TELEFON,
-			SIRKET_UNVAN	: $Sirket.val(),
-			SIRKET_ADRES	: $('#ADRES').val(),
-			SIRKET_LOGO 	: $('#LOGO').val(),
-			QR_CODE 		: '',
-			DOMAIN 			: $Sirket.attr('data-domain')
+			SIRKET_UNVAN	: $('option:selected', $('#SIRKET')).val(),
+			SIRKET_ADRES	: $Data.ADRES,
+			SIRKET_LOGO 	: $Data.LOGO,
+			DOMAIN 			: $Data.DOMAIN
 		}
 
 		setTimeout(function(){
@@ -92,7 +105,6 @@ jQuery(document).ready(function($) {
 				SIRKET_UNVAN	: 'Emard-DuBuque',
 				SIRKET_ADRES	: '90 Yuk Tsai Tsuen, New Territories, Bilzen, 18350',
 				SIRKET_LOGO 	: 'https://www.adjans.com.tr/content/themes/adjans_2020/images/adjans-black.svg',
-				QR_CODE 		: '',
 				DOMAIN 			: 'https://fake-address-nonexist.com/'
 			};
 
